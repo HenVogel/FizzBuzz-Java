d209595bdeca593928691ef6006849daa1a84c6e
Henry Vogel <145382058+HenVogel@users.noreply.github.com>
2025-01-15 09:20:44 -0500

    Add files via upload

---
+++ FizzBuzz Java UwU.java
@@ -0,0 +1,72 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    int x = 15;
+    int y = 5;
+    int z = 3;
+    int v = 19;
+
+
+    if (x % 15 == 0) {
+        System.out.println("Fizzbuzz");
+    }
+    else if (x % 5 == 0) {
+        System.out.println("Buzz");
+    }
+    else if (x % 3 == 0) {
+        System.out.println("Fizz");
+    }
+    else
+        System.out.println("Meow Meow Meow Crack Meow");
+    
+    if (y % 15 == 0) {
+        System.out.println("Fizzbuzz");
+    }
+    else if (y % 5 == 0) {
+        System.out.println("Buzz");
+    }
+    else if (y % 3 == 0) {
+        System.out.println("Fizz");
+    }
+    else
+        System.out.println("Meow Meow Meow Crack Meow");
+    
+    if (z % 15 == 0) {
+        System.out.println("Fizzbuzz");
+    }
+    else if (z % 5 == 0) {
+        System.out.println("Buzz");
+    }
+    else if (z % 3 == 0) {
+        System.out.println("Fizz");
+    }
+    else
+        System.out.println("Meow Meow Meow Crack Meow");
+    
+    if (v % 15 == 0) {
+        System.out.println("Fizzbuzz");
+    }
+    else if (v % 5 == 0) {
+        System.out.println("Buzz");
+    }
+    else if (v % 3 == 0) {
+        System.out.println("Fizz");
+    }
+    else
+        System.out.println("Meow Meow Meow Crack Meow");
+    
+  }
+}
